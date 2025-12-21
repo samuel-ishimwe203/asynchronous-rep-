0b1b1809dfd71bc5e7ec4d7f062223c5a004db51
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-12-21 19:55:19 +0200

    chore: event loops

--- synchronous.js
+++ synchronous.js
@@ -1,100 +1,113 @@
 
-// function blockForSecond(){
-//     const start=Date.now();
-//     while(Date.now()-start<1000){
+// // function blockForSecond(){
+// //     const start=Date.now();
+// //     while(Date.now()-start<1000){
 
-//     }
-// }
+// //     }
+// // }
 
-// console.log("start");
-// blockForSecond();
-// console.log('1 second later');
+// // console.log("start");
+// // blockForSecond();
+// // console.log('1 second later');
 
 
-// // function calls are also synchronous
+// // // function calls are also synchronous
 
-// function greet(){
-//     console.log("Hello!");
-// }
+// // function greet(){
+// //     console.log("Hello!");
+// // }
 
-// console.log("start");
-// greet();
-// console.log("end");
+// // console.log("start");
+// // greet();
+// // console.log("end");
 
-// call stack
+// // call stack
  
-function first(){
-    console.log('I am the first funciton');
-
-}
-
-function second(){
-    
-    console.log('I am the second function');
-    first();
-}
-
-second();
-
-
-// an other example
-
-function a(){
-    b();
-    console.log('Function A');
-}
-
-function b(){
-    c();
-    console.log('Function B');
-}
-function c(){
-    console.log('Function C');
-}
-
-a();
-
-// blocking 
-
-// console.log("Start");
-
-// while(true){
+// function first(){
+//     console.log('I am the first funciton');
 
 // }
-// console.log("End");
+
+// function second(){
+    
+//     console.log('I am the second function');
+//     first();
+// }
+
+// second();
 
 
-// non-blocking
+// // an other example
 
-console.log("I am student 1");
+// function a(){
+//     b();
+//     console.log('Function A');
+// }
+
+// function b(){
+//     c();
+//     console.log('Function B');
+// }
+// function c(){
+//     console.log('Function C');
+// }
+
+// a();
+
+// // blocking 
+
+// // console.log("Start");
+
+// // while(true){
+
+// // }
+// // console.log("End");
+
+
+// // non-blocking
+
+// console.log("I am student 1");
+// setTimeout(()=>{
+//     console.log(`This is samuel's message after 3 seconds`);
+
+// },3000);
+// console.log("nibyo san");
+// // synchronous callbacks 
+
+// function great(name, callback){
+//     console.log(`Hello , ${name}`);
+//     callback
+
+// }
+
+// great('Alice', ()=>{
+//     console.log("This is a callback function");
+// })
+
+
+// // simple code that shows how event loo works 
+
+// console.log("A")
+// setTimeout(()=>{
+//     console.log("B")
+// },0)
+
+// Promise.resolve().then(()=>{
+//     console.log("C")
+// })
+
+// console.log("D")
+
+// microtask inside the macrotask
+
 setTimeout(()=>{
-    console.log(`This is samuel's message after 3 seconds`);
+    console.log('A')
 
-},3000);
-console.log("nibyo san");
-// synchronous callbacks 
+    Promise.resolve().then(()=>{
+        console.log('B')
+    })
 
-function great(name, callback){
-    console.log(`Hello , ${name}`);
-    callback
-
-}
-
-great('Alice', ()=>{
-    console.log("This is a callback function");
-})
-
-
-// simple code that shows how event loo works 
-
-console.log("A")
-setTimeout(()=>{
-    console.log("B")
+    console.log("C")
 },0)
 
-Promise.resolve().then(()=>{
-    console.log("C")
-})
-
-console.log("D")
 
