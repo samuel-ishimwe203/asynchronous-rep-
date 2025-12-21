295ed4f9049d82f780dfd08ffb34d946d52e27ba
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-12-21 09:40:24 +0200

    chore: synchronous operation

--- synchronous.js
+++ synchronous.js
@@ -1,3 +1,11 @@
-console.log('This is the synchronous module.');
-console.log('It runs immediately when required.');
-console.log('No asynchronous operations here.');
+
+function blockForSecond(){
+    const start=Date.now();
+    while(Date.now()-start<1000){
+
+    }
+}
+
+console.log(start);
+blockForSecond();
+console.log('1 second later');
