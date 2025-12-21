7c542add78599e54991dd7f8eba4ac2e5ef36108
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-12-21 10:45:07 +0200

    chore: call stack in javascript

--- synchronous.js
+++ synchronous.js
@@ -1,25 +1,25 @@
 
-function blockForSecond(){
-    const start=Date.now();
-    while(Date.now()-start<1000){
+// function blockForSecond(){
+//     const start=Date.now();
+//     while(Date.now()-start<1000){
 
-    }
-}
+//     }
+// }
 
-console.log("start");
-blockForSecond();
-console.log('1 second later');
+// console.log("start");
+// blockForSecond();
+// console.log('1 second later');
 
 
-// function calls are also synchronous
+// // function calls are also synchronous
 
-function greet(){
-    console.log("Hello!");
-}
+// function greet(){
+//     console.log("Hello!");
+// }
 
-console.log("start");
-greet();
-console.log("end");
+// console.log("start");
+// greet();
+// console.log("end");
 
 // call stack
  
