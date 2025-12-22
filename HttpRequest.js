d2cd012d41d3294b061328ec35455da9d53ecc19
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-12-22 20:35:44 +0200

    chore: httpRequest data from server

--- HttpRequest.js
+++ HttpRequest.js
@@ -2,7 +2,9 @@
 const request= new XMLHttpRequest();
 
 request.addEventListener('readystatechange',()=>{
-    
+    if(request.readyState===4){
+        console.log(request.responseText);
+    }
 })
 request.open("GET","https://jsonplaceholder.typicode.com/todos/");
 
