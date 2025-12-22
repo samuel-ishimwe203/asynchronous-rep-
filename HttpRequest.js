f74afbb4a5313bcd0990a99c8957d97cd8d29e9b
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-12-22 22:07:40 +0200

    chore: json data create

--- HttpRequest.js
+++ HttpRequest.js
@@ -3,14 +3,15 @@ const doTask=(callback)=>{
     const request = new XMLHttpRequest()
 
 request.addEventListener('readystatechange',()=>{
-    const data= JSON.parse( request.responseText)
+    
     if(request.readyState===4 && request.status===200){
+        const data= JSON.parse(request.responseText);
         callback(undefined,data)
     }else if(request.readyState===4){
         callback('data not fetched', undefined)
     }
 })
-request.open('GET','todos.jso')
+request.open('GET','todos.json')
 request.send()
 
 }
