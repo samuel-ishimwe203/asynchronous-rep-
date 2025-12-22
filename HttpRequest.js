4f87416c1a8392649c2a03c70daae4946976b221
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-12-22 22:45:16 +0200

    chore: easy way of fetching data using promise

--- HttpRequest.js
+++ HttpRequest.js
@@ -41,7 +41,7 @@ request.addEventListener('readystatechange',()=>{
    
     if(request.readyState===4 && request.status===200){
          const data= JSON.parse( request.responseText)
-        callback(undefined,data)
+        callback(undefine,data)
     }else if(request.readyState===4){
         callback('data not fetched', undefined)
     }
