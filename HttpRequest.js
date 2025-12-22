be061682ca9b9f458240dc55df83931e69b65d16
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-12-22 22:44:58 +0200

    chore: easy way of fetching data using promise

--- HttpRequest.js
+++ HttpRequest.js
@@ -1,3 +1,38 @@
+// the way of fetching data using callback and callback hell
+// const doTask=(resource,callback)=>{
+//     const request = new XMLHttpRequest()
+
+// request.addEventListener('readystatechange',()=>{
+   
+//     if(request.readyState===4 && request.status===200){
+//          const data= JSON.parse( request.responseText)
+//         callback(undefined,data)
+//     }else if(request.readyState===4){
+//         callback('data not fetched', undefined)
+//     }
+// })
+// request.open('GET',resource)
+// request.send()
+
+// }
+
+
+// doTask('jsonData/todos.json',(error, data)=>{
+//     console.log(data)
+
+//     doTask('jsonData/murisa.json',(error,data)=>{
+//         console.log(data);
+//         doTask('jsonData/mucyo.json',(error, data)=>{
+//             console.log(data)
+//         })
+//     })
+// });
+
+
+// now here is upgraded easy way of fetching data using promise
+
+
+
 
 const doTask=(resource,callback)=>{
     const request = new XMLHttpRequest()
@@ -16,8 +51,4 @@ request.send()
 
 }
 
-
-doTask('jsonData/todos.json',(error, data)=>{
-    console.log(data)
-});
 
