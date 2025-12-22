c359da04b4da5db414c3944fb495471fad2ca8ae
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-12-22 21:10:34 +0200

    chore: httpRequest data from server

--- HttpRequest.js
+++ HttpRequest.js
@@ -1,9 +1,11 @@
-const request = new XMLHttpRequest();
+const request = new XMLHttpRequest()
 
 request.addEventListener('readystatechange',()=>{
-    if(request.readyState===4){
-        console.log(request.responseText)
+    if(request.readyState===4 && request.status===200){
+        console.log(request,request.responseText)
+    }else if(request.readyState===4){
+        console.log('data fatching failed')
     }
 })
-request.open('GET', "https://jsonplaceholder.typicode.com/todos/")
+request.open('GET','https://jsonplaceholder.typicode.com/todos/')
 request.send()
