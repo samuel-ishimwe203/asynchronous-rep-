a77d79ecf21000940ac7c753dfdf4f2379d7d0da
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-12-23 17:54:15 +0200

    chore: fetch data using async and await

--- HttpRequest.js
+++ HttpRequest.js
@@ -116,7 +116,7 @@ const promiseError= new Promise((resolve, reject)=>{
 const showError=async()=>{
     try{
         await promiseError
-    }catch(error){
+    }catch(er
         console.log(error)
     
     }
