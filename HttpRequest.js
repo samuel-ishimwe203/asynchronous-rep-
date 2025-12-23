2b00979f6376aa465041d2d89a712cb8b269d77e
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-12-23 15:00:55 +0200

    chore: fetch data using async and await

--- HttpRequest.js
+++ HttpRequest.js
@@ -89,7 +89,7 @@
 
 const getData= async()=>{
 
-    const response= await fetch('jsonData/murisa.json');
+    const response= await fetch('jsonData/mursa.json');
 
     if(response.status !==200){
         throw new Error('The data has been failed ')
