cc71da6641daf907b870c01a25183c652ea13522
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-12-23 21:06:47 +0200

    chore : setTimeout function

--- timeOutandInterval.js
+++ timeOutandInterval.js
@@ -1,27 +1,30 @@
 
 // setTimeout function
 
-function greet(name){
-    setTimeout(()=>{
-         console.log(`hello my name is ${name}`)
+// function greet(name){
+//     setTimeout(()=>{
+//          console.log(`hello my name is ${name}`)
 
-    },2000)
+//     },2000)
 
     
    
-}
+// }
 
-greet("samuel")
+// greet("samuel")
 
 // setInterval function
 
 function interval(){
  let a=1;
-
- for(a=1; a<=5;a++){
-    console.log(a)
+for(a=1; a<=5;a++){
+    if(a===4){
+        setInterval(()=>{
+   console.log(a)
+        },2000)
+    }
+ 
  }
 
 }
-
-greet()
+interval()
