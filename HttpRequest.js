b4c5038ef8f2cce1fdf637587c57119aaf845789
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-12-23 13:30:57 +0200

    fix: the http request using promise

--- HttpRequest.js
+++ HttpRequest.js
@@ -34,42 +34,6 @@
 
 
 
-const doTask=(resource)=>{
-
-          return new Promise((resolve , reject)=>{
-
-                     const request = new XMLHttpRequest()
-
-                     request.addEventListener('readystatechange',()=>{
-   
-                           if(request.readyState===4 && request.status===200){
-                                   const data= JSON.parse( request.responseText)
-                                   resolve(data)
-                            }else if(request.readyState===4){
-                              
-                              reject('failed , data not fetched')
-    }
-})
-
-request.open('GET',resource)
-request.send()
-
-    })
-
-
+const toDos=(resource)=>{
+    
 }
-
-doTask('jsonData/todos.json').then(result=>{
-    console.log('The promise1 resolved',result);
-    return doTask('jsonData/murisa.json')
-}).then(result=>{
-    console.log('The promise2 resolved ',result)
-
-    return doTask('jsonData/mucyo.json')
-}).then(result=>{
-    console.log('The promise3 rejected ',result)
-}).catch(error=>{
-    console.log('The promise rejected', error)
-})
-
-
