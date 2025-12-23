37d59b75d42d41290ad9858ceab3f37e7a8da4a0
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-12-23 22:03:12 +0200

    chore : promise static method

--- timeOutandInterval.js
+++ timeOutandInterval.js
@@ -37,3 +37,15 @@ function fooCall(namefn){
 }
 
 fooCall(foo)
+
+// promise static method
+
+const promise1= Promise.resolve(50)
+const promise2= 34;
+const promise3= new Promise((resolve, reject)=>{
+    setTimeout(resolve, 100, 'samuel')
+})
+
+Promise.all([promise1, promise2, promise3]).then(result=>
+    console.log(result)
+)
