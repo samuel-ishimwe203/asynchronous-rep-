7d0ed6c0fc860b2e161b1280437238e5250859a8
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-12-23 21:18:39 +0200

    chore : setTimeout function

--- timeOutandInterval.js
+++ timeOutandInterval.js
@@ -15,11 +15,25 @@
 
 // setInterval function
 
-function interval(){
- let a=1;
-for(a=1; a<=5;a++){
-    console.log(a)
- }
+// function interval(){
+//  let a=1;
+// for(a=1; a<=5;a++){
+//     console.log(a)
+//  }
 
+// }
+// interval()
+
+
+// callbacks
+ 
+function foo(name){
+    console.log(`Hello ${name}`)
 }
-interval()
+
+function fooCall(namefn){
+    const name='mucyo'
+    namefn=name
+}
+
+fooCall(foo)
