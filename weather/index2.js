705d16bfa7c2927d47009830c5bf30b8e9926872
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-12-23 17:34:11 +0200

    chore: weather cities app

--- weather/index2.js
+++ weather/index2.js
@@ -5,7 +5,9 @@ const queryWeather= async (city)=>{
 
         const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metrics`)
       if(!res.ok) throw new Error("City not found ");
-      
+
+      const data = await res.json();
+      displayWeather(data); 
 
     }catch(error){
 
