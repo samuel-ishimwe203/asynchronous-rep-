d41fd9f73fbd61fea80e987443006a454df6b7cc
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-12-24 14:51:30 +0200

    chore : fetching data

--- asynchronousRevision.js
+++ asynchronousRevision.js
@@ -169,8 +169,6 @@ fetch("https://jsonplaceholder.typicode.com/todos1").then(data=>{
   console.log("ooh no something want wrong")
 }).finally(()=>{
   console.log("Everything is done we have tried")
-});
-
 
 
 
