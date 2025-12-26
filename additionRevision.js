7d4c7ccca29f4ce9114c4649e113d517ceb3c7e4
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-12-27 00:00:34 +0200

    chore : promise

--- additionRevision.js
+++ additionRevision.js
@@ -109,7 +109,7 @@ orderFunction2(2000, ()=>console.log(`${stock.fruits[1]} was selected`))
     return orderFunction2(1000,()=>console.log(`${stock.liquid[0]} && ${stock.liquid[1]}`))
 })
 .then(()=>{
-    return orderFunction2(1000, ()=>console.logj('machine has started'))
+    return orderFunction2(1000, ()=>console.log('machine has started'))
 })
 .then(()=>{
     return orderFunction2(2000,()=>console.log(`the container is ${stock.holder[1]}`))
