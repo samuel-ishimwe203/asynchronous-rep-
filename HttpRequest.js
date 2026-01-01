0739f0475f3dfacf1ac81cff15e1f5a28d5d6af7
samuel-ishimwe203 <samuelishimw02@gmail.com>
2026-01-01 21:34:16 +0200

    chore: data fetching by async and await

--- HttpRequest.js
+++ HttpRequest.js
@@ -8,7 +8,7 @@ request.addEventListener('readystatechange',()=>{
          const data= JSON.parse( request.responseText)
         callback(undefined,data)
     }else if(request.readyState===4){
-        callback('data not fetched', undefined)
+        callback('data not fetched', undefsined)
     }
 })
 request.open('GET',resource)
