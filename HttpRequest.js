f7bf2b525c93911080e1ba736727239519c8801e
samuel-ishimwe203 <samuelishimw02@gmail.com>
2026-01-01 21:34:09 +0200

    chore: data fetching by async and await

--- HttpRequest.js
+++ HttpRequest.js
@@ -1,112 +1,112 @@
-// the way of fetching data using callback and callback hell
-// const doTask=(resource,callback)=>{
-//     const request = new XMLHttpRequest()
+the way of fetching data using callback and callback hell
+const doTask=(resource,callback)=>{
+    const request = new XMLHttpRequest()
 
-// request.addEventListener('readystatechange',()=>{
+request.addEventListener('readystatechange',()=>{
    
-//     if(request.readyState===4 && request.status===200){
-//          const data= JSON.parse( request.responseText)
-//         callback(undefined,data)
-//     }else if(request.readyState===4){
-//         callback('data not fetched', undefined)
-//     }
-// })
-// request.open('GET',resource)
-// request.send()
+    if(request.readyState===4 && request.status===200){
+         const data= JSON.parse( request.responseText)
+        callback(undefined,data)
+    }else if(request.readyState===4){
+        callback('data not fetched', undefined)
+    }
+})
+request.open('GET',resource)
+request.send()
 
-// }
-
-
-// doTask('jsonData/todos.json',(error, data)=>{
-//     console.log(data)
-
-//     doTask('jsonData/murisa.json',(error,data)=>{
-//         console.log(data);
-//         doTask('jsonData/mucyo.json',(error, data)=>{
-//             console.log(data)
-//         })
-//     })
-// });
+}
 
 
-// now here is upgraded easy way of fetching data using promise
+doTask('jsonData/todos.json',(error, data)=>{
+    console.log(data)
+
+    doTask('jsonData/murisa.json',(error,data)=>{
+        console.log(data);
+        doTask('jsonData/mucyo.json',(error, data)=>{
+            console.log(data)
+        })
+    })
+});
+
+
+now here is upgraded easy way of fetching data using promise
 
 
 
 
-// const toDos=(resource)=>{
+const toDos=(resource)=>{
 
-//     return new Promise((resolve, reject)=>{
+    return new Promise((resolve, reject)=>{
 
-//         const request =new XMLHttpRequest();
-//         request.addEventListener('readystatechange',()=>{
+        const request =new XMLHttpRequest();
+        request.addEventListener('readystatechange',()=>{
           
-//             if(request.readyState===4 && request.status===200){
-//                    const data= JSON.parse(request.responseText);
-//                    resolve(data)
-//             }else if(request.readyState===4){
-//                 reject('please failed to fetch data')
-//             }
+            if(request.readyState===4 && request.status===200){
+                   const data= JSON.parse(request.responseText);
+                   resolve(data)
+            }else if(request.readyState===4){
+                reject('please failed to fetch data')
+            }
                 
             
-//         })
+        })
 
-//         request.open('GET', resource);
-//         request.send()
-//     })
-// }
-
-
-// toDos('jsonData/murisa.json').then(data=>{
-//      console.log('promise resolved :', data)
-//      return toDos('jsonData/mucyo.json');
-
-// }).then(result=>{
-//     console.log('the promise 2 resolved', result)
-//     return toDos('jsonData/murisa.json')
-// }).then(result=>{
-//     console.log('promise 3 resolved ',result)
-// }).catch(error=>{
-//     console.log(error)
-
-// })
-
-// by fetching api
+        request.open('GET', resource);
+        request.send()
+    })
+}
 
 
-// fetch('jsonData/todos.json').then(response=>{
-//     console.log('resolved', response)
-//     return response.json()
-// }).then(data=>{
-//     console.log(data)
-// }).catch(error=>{
-//     console.log('rejected',error)
-// })
+toDos('jsonData/murisa.json').then(data=>{
+     console.log('promise resolved :', data)
+     return toDos('jsonData/mucyo.json');
+
+}).then(result=>{
+    console.log('the promise 2 resolved', result)
+    return toDos('jsonData/murisa.json')
+}).then(result=>{
+    console.log('promise 3 resolved ',result)
+}).catch(error=>{
+    console.log(error)
+
+})
+
+by fetching api
 
 
-// fetch data by using async and await
+fetch('jsonData/todos.json').then(response=>{
+    console.log('resolved', response)
+    return response.json()
+}).then(data=>{
+    console.log(data)
+}).catch(error=>{
+    console.log('rejected',error)
+})
 
 
-// const getData= async()=>{
+fetch data by using async and await
 
-//     const response= await fetch('jsonData/murisa.json');
 
-//     if(response.status !==200){
-//         throw new Error('The data has been failed ')
-//     }
-//     const data= await response.json();
+const getData= async()=>{
+
+    const response= await fetch('jsonData/murisa.json');
+
+    if(response.status !==200){
+        throw new Error('The data has been failed ')
+    }
+    const data= await response.json();
     
 
-//     return data
+    return data
 
     
-// }
+}
 
-// getData().then(data=>{
-//     console.log('resolved', data)
-// }).catch(error=>{
-//     console.log('rejected', error.message)
-// })
+getData().then(data=>{
+    console.log('resolved', data)
+}).catch(error=>{
+    console.log('rejected', error.message)
+})
 
 
 const promiseError= new Promise((resolve, reject)=>{
