a781f509387a66d38b2a260cbb1aa07f828663b0
samuel-ishimwe203 <samuelishimw02@gmail.com>
2026-01-01 09:17:05 +0200

    chore: callback hell

--- asynchronousRevision.js
+++ asynchronousRevision.js
@@ -147,7 +147,7 @@
 
 // function callbackHell(){
 //   setTimeout(()=>{
-//     console.log('hello')s
+//     console.log('hello')
 //     setTimeout(()=>{
 //       console.log('hello too how are you doing please?')
 //       setTimeout(()=>{
