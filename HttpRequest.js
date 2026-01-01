4dba46755c078e0f6f66f793d9ed4221b3ed1a15
samuel-ishimwe203 <samuelishimw02@gmail.com>
2026-01-01 21:43:34 +0200

    chore: error handling

--- HttpRequest.js
+++ HttpRequest.js
@@ -109,7 +109,7 @@ getData().then(data=>{
 })
 
 
-const promiseError= new Promise((resolve, reject)=>{
+const promiseError= new Promise((resolve, reject)=>{s
     reject('The error occured please')
 })
 
