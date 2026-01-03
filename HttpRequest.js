f560b372edab8473617ff9081b033399a92d0373
samuel-ishimwe203 <samuelishimw02@gmail.com>
2026-01-03 18:52:05 +0200

    chore: error handling

--- HttpRequest.js
+++ HttpRequest.js
@@ -115,7 +115,7 @@ const promiseError= new Promise((resolve, reject)=>{
 
 const showError=async()=>{
     try{
-        await promiseErrord
+        await promiseError
     }catch(error){
         console.log(error);
     
