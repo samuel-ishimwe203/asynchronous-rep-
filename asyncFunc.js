103855b63b99c624c65e371bdc604b8c0924654d
samuel-ishimwe203 <samuelishimw02@gmail.com>
2026-01-13 11:20:20 +0200

    chore: call apis

--- asyncFunc.js
+++ asyncFunc.js
@@ -2,7 +2,7 @@
 function getData(){
     return new Promise(async(resolve, reject)=>{
         try {
-            let data= await fetch("mucyo.json");
+            let data= await fetch("mucyo.js");
             if(!data.ok) throw new Error ('failed to fetch data');
             let users= await data.json()
             resolve(users)
