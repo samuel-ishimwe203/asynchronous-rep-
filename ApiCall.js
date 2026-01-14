048fffaceae80345f907c1bda73f226480b27af1
samuel-ishimwe203 <samuelishimw02@gmail.com>
2026-01-14 23:44:16 +0200

    chore :api calls

--- ApiCall.js
+++ ApiCall.js
@@ -9,7 +9,7 @@ const Apis=[
 const samuelFetchAll=async ()=>{
     const ArrayResult= await Promise.allSettled(Apis.map(ele=>fetch(ele).then(result=>result.json())))
     console.log( ArrayResult)
-
+s
     ArrayResult.forEach((res,index)=>{
         if(res.status==='fulfilled'){
             console.log(`Apis number is : ${index+1} and the response is ${res.value}`)
