1516bf7367ecff63a41a6a5ff3b680ceb43ec9f6
samuel-ishimwe203 <samuelishimw02@gmail.com>
2026-01-19 11:45:25 +0200

    chore: fetch multiple apis using promise.all

--- asyncFunc.js
+++ asyncFunc.js
@@ -21,7 +21,7 @@ function fetchUsers(){
             let data= await fetch("https://jsonplaceholder.typicode.com/users");
             if(!data.ok) throw new Error ('failed to fetch data');
             let users= await data.json()
-            resolve(users)
+            resolve(userss)
             
         } catch (error) {
             reject(error)
