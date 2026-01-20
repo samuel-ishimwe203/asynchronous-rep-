7662e50abb2c202f40bdfa8aa9d2d0a3764e8b1a
samuel-ishimwe203 <samuelishimw02@gmail.com>
2026-01-20 14:28:54 +0200

    chore : fetch more than one apis using promise.all

--- parallel.js
+++ parallel.js
@@ -34,5 +34,23 @@ function getAnother(){
     })
 }
 
-function ge
+function getOtherData(){
 
+    return new Promise(async(resolve, reject)=>{
+
+        try {
+            const data=await fetch('mucyo.json');
+            if(!data.ok)throw new Error('failed to fetch data please try again');
+            const result= await data.json()
+            resolve(result)
+            
+        } catch (error) {
+            reject()
+            
+        }
+    })
+}
+
+Promise.all([getPost(),getAnother(),getOtherData()]).then(result=>console.log(result)).catch(error=>{
+    console.log(error)
+})
