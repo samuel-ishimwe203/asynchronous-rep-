db6e4a3c617417feffe5d961fb2aa517cdf820cb
samuel-ishimwe203 <samuelishimw02@gmail.com>
2026-01-20 14:41:21 +0200

    chore: asynchronous and paraller programming

--- parallel.js
+++ parallel.js
@@ -1,56 +1,11 @@
+// asynchronous and parallel programming
 
- function getPost(){
+// Example of asynchronous 
 
+console.log('start');
 
-return new Promise(async(resolve, reject)=>{
-
-    try {
-         const data = await fetch('https://jsonplaceholder.typicode.com/posts');
-         if(!data.ok) throw new Error('failed to fetch data');
-         const result=await data.json()
-         resolve(result)
-        
-    } catch (error) {
-        reject(error)
-        
-    }
+setTimeout(()=>{
+    console.log('now async is finished')
 })
-    }
 
-function getAnother(){
-    return new Promise(async(resolve, reject)=>{
-        try {
-
-            const response= await fetch('https://jsonplaceholder.typicode.com/users')
-            if(!response.ok) throw new Error('failed to fetch');
-
-            const result=await response.json()
-            resolve(result);
-            
-        } catch (error) {
-            reject(error)
-            
-        }
-    })
-}
-
-function getOtherData(){
-
-    return new Promise(async(resolve, reject)=>{
-
-        try {
-            const data=await fetch('mucyo.json');
-            if(!data.ok)throw new Error('failed to fetch data please try again');
-            const result= await data.json()
-            resolve(result)
-            
-        } catch (error) {
-            reject()
-            
-        }
-    })
-}
-
-Promise.all([getPost(),getAnother(),getOtherData()]).then(result=>console.log(result)).catch(error=>{
-    console.log(error)
-})
+console.log('End')
