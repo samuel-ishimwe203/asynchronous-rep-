e1d2d36cbf513311db12ec116c3960c1e6187dc7
samuel-ishimwe203 <samuelishimw02@gmail.com>
2026-01-22 12:58:58 +0200

    chore: how promise working

--- promise.js
+++ promise.js
@@ -1,4 +1,28 @@
-// promise is an object the execute event completion or fielded
+/* 
+    - promise is an object the execute event completion or fielded
 
-// promise state -pending -fulfied -rejected 
+* promise state :
+
+ -pending: initially when  state value is pending. 
+ -fulfied: when promise is resolved.
+ -rejected: when promise rejected.
+ 
+ * promise result
+
+  - Undefined: initially when state value is pending.
+  - value: The value you get after promise resolved.
+  -Errror: The when promise is rejected rejected.
+
+  * promise handling 
+
+  - .then : It handle the promise when it resolved or rejected but it usualy used to resolved it.
+  - .catch : It handle the promise when it is only rejected.
+  - .finally:
+
+*/
+
+
+const promise= new Promise(function(resolve, reject){
+    return resolve('I am student in tumba college')
+})
 
