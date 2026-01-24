7350870a4b61b7049f06aae2b4802aa5cd9a7acb
samuel-ishimwe203 <samuelishimw02@gmail.com>
2026-01-24 20:01:53 +0200

    chore: To fetch more than one Apis

---
+++ fetchApi.js
@@ -0,0 +1,65 @@
+
+async function getUser() {
+
+    try {
+               const fetch1= await fetch('https://jsonplaceholder.typicode.com/users')
+               if(!fetch1.ok) throw new Error('failed to fetch data');
+               const data = await fetch1.json();
+              setTimeout(()=>{
+
+                console.log(data)
+
+              },1000) 
+            
+        } catch (error) {
+            console.log(error)
+            
+        }
+     }
+
+     getUser()
+
+
+     async function getTodos() {
+
+    try {
+               const fetch1= await fetch('https://jsonplaceholder.typicode.com/todos')
+               if(!fetch1.ok) throw new Error('failed to fetch data');
+               const data = await fetch1.json();
+                 setTimeout(()=>{
+
+                console.log(data)
+
+              },1000) 
+            
+        } catch (error) {
+            console.log(error)
+            
+        }
+     }
+
+   getTodos()
+
+
+
+    async function getComment() {
+
+    try {
+               const fetch1= await fetch('https://jsonplaceholder.typicode.com/comments')
+               if(!fetch1.ok) throw new Error('failed to fetch data');
+               const data = await fetch1.json();
+                 setTimeout(()=>{
+
+                console.log(data)
+
+              },1000) 
+            
+        } catch (error) {
+            console.log(error)
+            
+        }
+     }
+
+   getComment()
+
+    
--- setInterval.js
+++ setInterval.js
@@ -1,19 +1,50 @@
 
-let interval;
-function intervals(){
-    interval=setInterval(()=>{
+// let interval;
+// function intervals(){
+//     interval=setInterval(()=>{
 
-        console.log("hello")
+//         console.log("hello")
 
-    },2000)
+//     },2000)
 
     
+// }
+
+// intervals()
+
+
+// setTimeout(()=>{
+//     clearInterval(interval)
+//     console.log('Interval cleared')
+// },10000)
+
+
+async function funct1(){
+    setTimeout(()=>{
+        console.log('A')
+    },1000)
 }
 
-intervals()
+funct1()
+
+async function funct2(){
+    setTimeout(()=>{
+        console.log('')
+    },1000)
+}
 
 
-setTimeout(()=>{
-    clearInterval(interval)
-    console.log('Interval cleared')
-},10000)
+
+
+funct2()
+
+async function funct3(){
+    setTimeout(()=>{
+        console.log('B')
+    },1000)
+}
+
+
+funct3()
+
+
