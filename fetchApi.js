4dcaf0838ae15f8236757aee38e89f798ce3820e
samuel-ishimwe203 <samuelishimw02@gmail.com>
2026-01-24 21:07:35 +0200

    chore: handle fetching with more than two apis

--- fetchApi.js
+++ fetchApi.js
@@ -55,7 +55,7 @@ async function getUser() {
      }
 
 async function FetchAll (){
-    const res = Promise.ra([ getUser(),getTodos(),getComment()])
+    const res = Promise.r([ getUser(),getTodos(),getComment()])
     return res;
 }
 
