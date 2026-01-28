5e564bf29d01515c303d2034cb824c0675d1ac61
samuel-ishimwe203 <samuelishimw02@gmail.com>
2026-01-28 18:20:55 +0200

    chore: eventEmitter

--- AllTasks/eventEmitter.js
+++ AllTasks/eventEmitter.js
@@ -2,7 +2,7 @@
 const emitter= require("events");
 const eventEmitter= new emitter();
 
-eventEmitter.on('login',(user)=>{
+eventEmitter.on('logizn',(user)=>{
 console.log(`${user} logged in please`)
 })
 
